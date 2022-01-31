1905ad31daacffab305c5b51301cfa1a39b6c9d1
Anita <anitapopova1996@gmail.com>
2022-01-31 14:07:31 +0200

    testing.

---
+++ src/main/java/com/study/TestConnection.java
@@ -0,0 +1,21 @@
+package com.study;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class TestConnection {
+    public static void main(String[] args) {
+        StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+        Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
+
+        SessionFactory factory = meta.getSessionFactoryBuilder().build();
+        Session session = factory.openSession();
+
+        session.close();
+        factory.close();
+    }
+}
