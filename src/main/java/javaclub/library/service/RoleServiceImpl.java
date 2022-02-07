47efc368f0f7011385fbd6b036b62ea3617be92f
ZSergo <zapadenko.s@gmail.com>
2022-02-07 19:12:01 +0200

    edit RoleServiceImpl

--- src/main/java/javaclub/library/service/RoleServiceImpl.java
+++ src/main/java/javaclub/library/service/RoleServiceImpl.java
@@ -1,32 +1,50 @@
 package javaclub.library.service;
 
+import javaclub.library.dao.RoleDAO;
 import javaclub.library.model.Role;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Service
 public class RoleServiceImpl implements RoleService{
+
+    @Autowired
+    private RoleDAO roleDAO;
+
+    public void setRoleDAO(RoleDAO roleDAO) {
+        this.roleDAO = roleDAO;
+    }
+
     @Override
+    @Transactional
     public void addRole(Role role) {
-
+        this.roleDAO.addRole(role);
     }
 
     @Override
+    @Transactional
     public void updateRole(Role role) {
-
+        this.roleDAO.updateRole(role);
     }
 
     @Override
+    @Transactional
     public List<Role> listRoles() {
-        return null;
+        return this.roleDAO.listRoles();
     }
 
     @Override
+    @Transactional
     public Role getRoleById(int id) {
-        return null;
+        return this.roleDAO.getRoleById(id);
     }
 
     @Override
+    @Transactional
     public void removeRole(int id) {
-
+        this.roleDAO.removeRole(id);
     }
 }
