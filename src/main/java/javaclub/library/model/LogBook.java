cf479740689fa5a2e90b6bb1a518f3a7ba1fd9ff
ZSergo <zapadenko.s@gmail.com>
2022-02-07 22:00:53 +0200

    Add logBook model

---
+++ src/main/java/javaclub/library/model/LogBook.java
@@ -0,0 +1,53 @@
+package javaclub.library.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "logBook")
+public class LogBook {
+    @Embeddable
+    public static class Id implements Serializable {
+        @Column(name = "idBook")
+        int idBook;
+
+        @Column(name = "idUser")
+        int idUser;
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Id id = (Id) o;
+            return idUser == id.idUser &&
+                    idBook == id.idBook;
+        }
+
+        @Override
+        public int hashCode() {
+            return idUser + idBook;
+        }
+    }
+
+    @EmbeddedId
+    Id id;
+
+    @ManyToOne
+    @JoinColumn(name = "idUser")
+    User user;
+
+    @ManyToOne
+    @JoinColumn(name = "idBook")
+    Book book;
+
+    @Column(name = "score")
+    int score;
+
+    @Column (name = "dataOut")
+    private LocalDateTime dataOut ;
+
+    @Column (name = "dataIn")
+    private LocalDateTime dataIn ;
+}
+
