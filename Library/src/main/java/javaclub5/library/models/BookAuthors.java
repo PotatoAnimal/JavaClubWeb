dd411b6b300eb9518e1e64f32485a9c1bad0cb88
Anita <anitapopova1996@gmail.com>
2022-02-09 16:57:44 +0200

    added getters setters

--- Library/src/main/java/javaclub5/library/models/BookAuthors.java
+++ Library/src/main/java/javaclub5/library/models/BookAuthors.java
@@ -6,13 +6,28 @@ import javax.persistence.*;
 @Embeddable
 @Table(name = "booksAuthors", schema = "library", catalog = "JavaClubWeb")
 public class BookAuthors {
-
+    @Id
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "idBook", insertable = false, updatable = false)
     private Book idBook;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "idAuthor")
+    @JoinColumn(name = "idAuthor",insertable = false, updatable = false)
     private Author idAuthor;
 
+    public Book getIdBook() {
+        return idBook;
+    }
+
+    public void setIdBook(Book idBook) {
+        this.idBook = idBook;
+    }
+
+    public Author getIdAuthor() {
+        return idAuthor;
+    }
+
+    public void setIdAuthor(Author idAuthor) {
+        this.idAuthor = idAuthor;
+    }
 }
