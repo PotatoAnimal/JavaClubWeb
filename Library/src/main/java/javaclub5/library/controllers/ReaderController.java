1cb5b7b66adc587b2682ad6e31e850ccdfab846f
Rostyslav <rostylka96@gmail.com>
2022-02-16 17:25:54 +0200

    Some Code

--- Library/src/main/java/javaclub5/library/controllers/ReaderController.java
+++ Library/src/main/java/javaclub5/library/controllers/ReaderController.java
@@ -1,5 +1,6 @@
 package javaclub5.library.controllers;
 
+import javaclub5.library.dao.UserDao;
 import javaclub5.library.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,17 +12,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 
-@RequestMapping("/readers")
 public class ReaderController {
     @Autowired
     private UserService userService;
+    @Autowired
+    private UserDao userDao;
 
-    @PostMapping("/{id}")
+    @PostMapping("/readers/{id}")
     public String goToReader(@PathVariable("id") int id, Model model) {
         model.addAttribute("books", userService.getBookList());
         model.addAttribute("userService", userService);
+        model.addAttribute("user", userDao.readByID(id));
         model.addAttribute("id", id);
         return "readers/readerbookslist";
+    }
+
+    @PostMapping("/users/{id}")
+    public String editReader(@PathVariable("id") int id, Model model) {
+        model.addAttribute("user", userDao.readByID(id));
+        model.addAttribute("id", id);
+        return "readers/readersedit";
+    }
+
+    @PostMapping("/reader/update")
+    public String update(Model model) {
+        model.addAttribute("user", userDao.readByID(id));
+        return "redirect: /readers/" + user.getId());
     }
 
 }
