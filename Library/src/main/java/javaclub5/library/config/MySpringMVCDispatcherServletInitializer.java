10036495cfaef444c8064551c5e4b031cfcb6590
ZSergo <zapadenko.s@gmail.com>
2022-02-16 19:21:01 +0200

    changes for using th:method="PATCH"

--- Library/src/main/java/javaclub5/library/config/MySpringMVCDispatcherServletInitializer.java
+++ Library/src/main/java/javaclub5/library/config/MySpringMVCDispatcherServletInitializer.java
@@ -1,6 +1,10 @@
 package javaclub5.library.config;
 
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 public class MySpringMVCDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
@@ -16,5 +20,16 @@ public class MySpringMVCDispatcherServletInitializer extends AbstractAnnotationC
     @Override
     protected String[] getServletMappings() {
         return new String[] {"/"};
+    }
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        super.onStartup(servletContext);
+        registerHiddenFieldFilter(servletContext);
+    }
+
+    private void registerHiddenFieldFilter(ServletContext context){
+        context.addFilter("hiddenHttpMethodFilter",
+                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null,true,"/*");
     }
 }
