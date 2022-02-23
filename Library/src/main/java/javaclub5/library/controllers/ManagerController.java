cc61b0661f2bd18a15b6f368ef12633c706d34e3
Rostylka <rostylka96@gmail.com>
2022-02-23 23:02:10 +0200

    Some Code

--- Library/src/main/java/javaclub5/library/controllers/ManagerController.java
+++ Library/src/main/java/javaclub5/library/controllers/ManagerController.java
@@ -1,11 +1,13 @@
 package javaclub5.library.controllers;
 
 import javaclub5.library.dao.BookDao;
+import javaclub5.library.dao.RegBooksDao;
 import javaclub5.library.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ManagerController {
@@ -13,10 +15,24 @@ public class ManagerController {
     UserService userService;
     @Autowired
     BookDao bookDao;
+    @Autowired
+    RegBooksDao regBooksDao;
     @GetMapping("/managerProfile")
     public String managerProfile(Model model) {
         model.addAttribute("userService", userService);
         model.addAttribute("books", bookDao.readAll());
         return "manager/managerProfile";
+    }
+
+    @PostMapping("/managerRegbooks")
+    public String getManagerRegbooks(Model model) {
+        model.addAttribute("regbooks", regBooksDao.readAll());
+        return "manager/managerregbooks";
+    }
+
+    @PostMapping("/addNewRegistration")
+    public String addNewRegbook(Model model) {
+        model.addAttribute("regbooks", regBooksDao.readAll());
+        return "manager/managerregbooks";
     }
 }
