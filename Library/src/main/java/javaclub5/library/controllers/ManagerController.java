d19e8153370f56193b9431c9c41149709bc68b68
Rostylka <rostylka96@gmail.com>
2022-02-23 20:22:29 +0200

    Some Code

--- Library/src/main/java/javaclub5/library/controllers/ManagerController.java
+++ Library/src/main/java/javaclub5/library/controllers/ManagerController.java
@@ -1,12 +1,22 @@
 package javaclub5.library.controllers;
 
+import javaclub5.library.dao.BookDao;
+import javaclub5.library.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ManagerController {
+    @Autowired
+    UserService userService;
+    @Autowired
+    BookDao bookDao;
     @GetMapping("/managerProfile")
-    public String managerProfile() {
+    public String managerProfile(Model model) {
+        model.addAttribute("userService", userService);
+        model.addAttribute("books", bookDao.readAll());
         return "manager/managerProfile";
     }
 }
