a50bcb4e32799127f2f7acf2e3052a907c388a4c
Rostylka <rostylka96@gmail.com>
2022-03-14 21:49:51 +0200

    Some Code

--- Library/src/main/java/javaclub5/library/controllers/ManagerController.java
+++ Library/src/main/java/javaclub5/library/controllers/ManagerController.java
@@ -1,13 +1,22 @@
 package javaclub5.library.controllers;
 
+import javaclub5.library.dao.AuthorDao;
 import javaclub5.library.dao.BookDao;
 import javaclub5.library.dao.RegBooksDao;
+import javaclub5.library.models.Author;
+import javaclub5.library.models.Book;
+import javaclub5.library.models.RegBooks;
+import javaclub5.library.models.User;
 import javaclub5.library.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class ManagerController {
@@ -17,6 +26,8 @@ public class ManagerController {
     BookDao bookDao;
     @Autowired
     RegBooksDao regBooksDao;
+    @Autowired
+    AuthorDao authorDao;
     @GetMapping("/managerProfile")
     public String managerProfile(Model model) {
         model.addAttribute("userService", userService);
@@ -24,15 +35,32 @@ public class ManagerController {
         return "manager/managerProfile";
     }
 
-    @PostMapping("/managerRegbooks")
+    @GetMapping("/managerRegbooks")
     public String getManagerRegbooks(Model model) {
         model.addAttribute("regbooks", regBooksDao.readAll());
         return "manager/managerregbooks";
     }
 
     @PostMapping("/addNewRegistration")
-    public String addNewRegbook(Model model) {
-        model.addAttribute("regbooks", regBooksDao.readAll());
-        return "manager/managerregbooks";
+    public String addNewRegbook(@ModelAttribute("book") Book book,
+                                @ModelAttribute("author") Author author,
+                                @ModelAttribute("regbooks") RegBooks regBooks,
+                                Model model) {
+        return "manager/manageraddnewregbook";
+    }
+
+    @PostMapping("/register")
+    public String registerRegbook(@ModelAttribute("book") Book book,
+                                @ModelAttribute("author") Author author,
+                                @ModelAttribute("regbooks") RegBooks regBooks,
+                                Model model) {
+        authorDao.addAuthor(author);
+        List<Author> authorList = new ArrayList<>();
+        authorList.add(author);
+        book.setAuthors(authorList);
+        bookDao.addBook(book);
+        regBooks.setBook(book);
+        regBooksDao.create(regBooks);
+        return "redirect: /managerRegbooks";
     }
 }
